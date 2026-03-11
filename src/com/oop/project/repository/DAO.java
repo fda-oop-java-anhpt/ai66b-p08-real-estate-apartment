5b79c85e68af60242deac11a64630e83929772d2
thangkaka <dinhthangnguyen260106@gmail.com>
2026-03-12 01:16:08 +0700

    Create Data Access Object (DAO) interface
    Data Access Object (DAO) interface is implemented in classes that execute SQL statements and mainly update database and return POJOs.

---
+++ src/com/oop/project/repository/DAO.java
@@ -0,0 +1,11 @@
+package com.oop.project.repository;
+
+/*
+ * Data Access Object (DAO) interface.
+ * Implemented in classes that execute SQL statements.
+ * Mainly update database or return POJOs.
+ */
+
+public interface DAO {
+    
+}
