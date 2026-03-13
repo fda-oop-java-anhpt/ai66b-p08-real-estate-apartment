ae01babd20522c01966f23a34fd3970946d10da9
thangkaka <dinhthangnguyen260106@gmail.com>
2026-03-13 13:42:38 +0700

    Create bussiness logic authentication for login action

---
+++ src/com/oop/project/service/LoginAuthentication.java
@@ -0,0 +1,30 @@
+package com.oop.project.service;
+
+import com.oop.project.repository.UserRepository;
+import com.oop.project.model.User;
+import com.oop.project.exception.AuthenticationException;
+
+/*  
+ * Bussiness logic for Login action
+ */
+
+public class LoginAuthentication {
+    
+    public static User getUser(String username, String password, String role) throws Exception {
+        try {
+            User userLogin = new UserRepository(username, password, role).login();
+            
+            // User is found (not null)
+            if (userLogin != null) {
+                return userLogin;
+                
+            // User is not found (null)
+            } else {
+                throw new AuthenticationException("User not found! Wrong username or password.");
+            }
+            
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+}
