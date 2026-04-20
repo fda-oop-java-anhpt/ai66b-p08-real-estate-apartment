a3090c92a1a308571fce4a51919af827f93d73b5
thangkaka <dinhthangnguyen260106@gmail.com>
2026-04-20 13:40:40 +0700

    Refactor Amenity class structure and comments
    Refactor Amenity class by improving comments and formatting.

--- src/com/oop/project/model/Amenity.java
+++ src/com/oop/project/model/Amenity.java
@@ -1,28 +1,30 @@
+// Amenity.java
 package com.oop.project.model;
-/*
+
+/**
  * Plain Old Java Object for entities from 'amenities' table.
  */
 public class Amenity implements POJO {
     private final int id;
     private final String name;
-    
-    // Constructor method
+
+    // Constructor
     public Amenity(int id, String name) {
         this.id = id;
         this.name = name;
     }
-    
-    
-    // Getter methods
+
+    // Getters
     @Override
-    public int getId() {return id;}
-    public String getName() {return name;}
-    
-    
-    // No setter method
-    
-    
-    // toString method
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    // toString
     @Override
     public String toString() {
         return "Amenity(" + getId() + ", " + getName() + ")";
