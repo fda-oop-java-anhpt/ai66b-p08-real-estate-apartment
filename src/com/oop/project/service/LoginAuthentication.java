1b85aab4bb93fd982dcc28e36eac4590c71b9366
thangkaka <dinhthangnguyen260106@gmail.com>
2026-04-20 21:48:58 +0700

    Enhance login logic with session management
    Added session management to store the logged-in user.

--- src/com/oop/project/service/LoginAuthentication.java
+++ src/com/oop/project/service/LoginAuthentication.java
@@ -3,6 +3,7 @@ package com.oop.project.service;
 import com.oop.project.repository.UserRepository;
 import com.oop.project.model.User;
 import com.oop.project.exception.AuthenticationException;
+import com.oop.project.util.SessionManager;
 
 /*  
  * Bussiness logic for Login action
@@ -13,16 +14,12 @@ public class LoginAuthentication {
     public static User getUser(String username, String password, String role) throws Exception {
         try {
             User userLogin = new UserRepository(username, password, role).login();
-            
-            // User is found (not null)
             if (userLogin != null) {
+                SessionManager.setCurrentUser(userLogin); // Store logged-in user
                 return userLogin;
-                
-            // User is not found (null)
             } else {
                 throw new AuthenticationException("User not found! Wrong username or password.");
             }
-            
         } catch (Exception e) {
             throw e;
         }
