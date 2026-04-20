744ccb00825dcb58c04dce6bb1c840df3810dd03
thangkaka <dinhthangnguyen260106@gmail.com>
2026-04-20 12:07:09 +0700

    Refactor DBConnection to use environment variables

--- src/com/oop/project/db/DBConnection.java
+++ src/com/oop/project/db/DBConnection.java
@@ -1,6 +1,8 @@
 package com.oop.project.db;
 
 import java.sql.*;
+import java.util.Map;
+import com.oop.project.util.ReadEnv;
 
 /*
 * This is used to establish a connection to MySQL database.
@@ -12,16 +14,22 @@ public class DBConnection {
     * Connection URL composition: jdbc:mysql://Hostname:Port/your_schema
     * Left click the connection in MySQL, then choose 'Edit connection' to see your parameters.
     */
+    private static final Map<String,String> envMap = new ReadEnv("/com/oop/project/config/Connection.env").load();
     
-    private final String url = "jdbc:mysql://Hostname:Port/your_schema";
-    private final String username = "root";
-    private final String password = "";
+    private static final String HOST = envMap.getOrDefault("HOST", "localhost");
+    private static final String PORT = envMap.getOrDefault("PORT", "3306");
+    private static final String SCHEMA = envMap.getOrDefault("SCHEMA", "");
+    
+    private static final String URL = String.format("jdbc:mysql://%s:%s/%s", HOST, PORT, SCHEMA);
+    private static final String USERNAME = envMap.getOrDefault("USERNAME", "root");
+    private static final String PASSWORD = envMap.getOrDefault("PASSWORD", "");
+    
     
     public DBConnection() {}
     
     // This method is to establish a connection.
     public Connection establish() throws SQLException {
         // return a connection if valid, else SQLException Error.
-        return DriverManager.getConnection(url, username, password);
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
         }
 }
