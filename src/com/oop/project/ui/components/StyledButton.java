60a2d020869456e91891ce528cdb7407b93b4a0e
thangkaka <dinhthangnguyen260106@gmail.com>
2026-04-22 23:47:15 +0700

    Enhance StyledButton with pressed state and radius

--- src/com/oop/project/ui/components/StyledButton.java
+++ src/com/oop/project/ui/components/StyledButton.java
@@ -8,36 +8,69 @@ import java.awt.event.MouseEvent;
 public class StyledButton extends JButton {
     private Color normalColor;
     private Color hoverColor;
+    private Color pressedColor;
+    private int radius;
 
     public StyledButton(String text, Color backgroundColor) {
+        this(text, backgroundColor, 8);
+    }
+
+    public StyledButton(String text, Color backgroundColor, int radius) {
         super(text);
         this.normalColor = backgroundColor;
-        this.hoverColor = backgroundColor.darker();
+        this.hoverColor = backgroundColor.brighter();
+        this.pressedColor = backgroundColor.darker();
+        this.radius = radius;
 
         setFont(new Font("Segoe UI", Font.BOLD, 14));
         setForeground(Color.WHITE);
         setBackground(normalColor);
         setBorderPainted(false);
         setFocusPainted(false);
+        setContentAreaFilled(false);
+        setOpaque(false);
         setCursor(new Cursor(Cursor.HAND_CURSOR));
-        setPreferredSize(new Dimension(120, 40));
+        setPreferredSize(new Dimension(120, 36));
 
         addMouseListener(new MouseAdapter() {
             @Override
             public void mouseEntered(MouseEvent e) {
                 setBackground(hoverColor);
+                repaint();
             }
 
             @Override
             public void mouseExited(MouseEvent e) {
                 setBackground(normalColor);
+                repaint();
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                setBackground(pressedColor);
+                repaint();
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                setBackground(hoverColor);
+                repaint();
             }
         });
     }
 
+    @Override
+    protected void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g.create();
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setColor(getBackground());
+        g2.fillRoundRect(0, 0, getWidth(), getHeight(), radius, radius);
+        super.paintComponent(g2);
+        g2.dispose();
+    }
+
     public void setNormalColor(Color color) {
         this.normalColor = color;
-        this.hoverColor = color.darker();
-        setBackground(normalColor);
+        setBackground(color);
     }
 }
