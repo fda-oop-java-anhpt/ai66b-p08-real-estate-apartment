e434dad0aa715218c3ff43574b1d4c2eede77127
thangkaka <dinhthangnguyen260106@gmail.com>
2026-05-01 21:12:58 +0700

    Remove main method from LoginScreen
    Removed the main method from LoginScreen class.

--- src/com/oop/project/ui/LoginScreen.java
+++ src/com/oop/project/ui/LoginScreen.java
@@ -359,13 +359,4 @@ public class LoginScreen extends JFrame {
             }
         }
     }
-
-    public static void main(String[] args) {
-        try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        SwingUtilities.invokeLater(() -> new LoginScreen().setVisible(true));
-    }
 }
