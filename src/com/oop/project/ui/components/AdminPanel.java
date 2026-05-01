1782bc0a5b5ddcb39b32f2f00ed5300d117eed2f
thangkaka <dinhthangnguyen260106@gmail.com>
2026-05-01 21:14:57 +0700

    Refactor AdminPanel to use JTabbedPane

--- src/com/oop/project/ui/components/AdminPanel.java
+++ src/com/oop/project/ui/components/AdminPanel.java
@@ -1,29 +1,27 @@
 package com.oop.project.ui.components;
 
-import com.oop.project.ui.Theme;
-
 import javax.swing.*;
-import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 public class AdminPanel extends JPanel {
-    private JTable favoritesTable;
-    private StyledButton removeButton;
-
     public AdminPanel() {
         setLayout(new BorderLayout());
-        setBackground(Theme.BACKGROUND);
-        favoritesTable = new JTable();
-        favoritesTable.setFont(Theme.BODY_FONT);
-        favoritesTable.setRowHeight(30);
-        removeButton = new StyledButton("Remove", Theme.DANGER);
+        JTabbedPane adminTabs = new JTabbedPane();
+        adminTabs.addTab("Audit Logs", new AuditLogPanel());
+        adminTabs.addTab("Login History", new LoginHistoryPanel());
+        add(adminTabs, BorderLayout.CENTER);
+    }
+}package com.oop.project.ui.components;
 
-        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-        buttonPanel.setBackground(Theme.SURFACE);
-        buttonPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
-        buttonPanel.add(removeButton);
+import javax.swing.*;
+import java.awt.*;
 
-        add(buttonPanel, BorderLayout.NORTH);
-        add(new JScrollPane(favoritesTable), BorderLayout.CENTER);
+public class AdminPanel extends JPanel {
+    public AdminPanel() {
+        setLayout(new BorderLayout());
+        JTabbedPane adminTabs = new JTabbedPane();
+        adminTabs.addTab("Audit Logs", new AuditLogPanel());
+        adminTabs.addTab("Login History", new LoginHistoryPanel());
+        add(adminTabs, BorderLayout.CENTER);
     }
 }
