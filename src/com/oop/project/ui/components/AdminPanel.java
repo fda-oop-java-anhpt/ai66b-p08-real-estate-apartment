9cfd79e92a026fc115c6b6f37b82fc12db336fba
thangkaka <dinhthangnguyen260106@gmail.com>
2026-05-04 16:43:08 +0700

    Remove duplicate code in AdminPanel.java

--- src/com/oop/project/ui/components/AdminPanel.java
+++ src/com/oop/project/ui/components/AdminPanel.java
@@ -11,17 +11,4 @@ public class AdminPanel extends JPanel {
         adminTabs.addTab("Login History", new LoginHistoryPanel());
         add(adminTabs, BorderLayout.CENTER);
     }
-}package com.oop.project.ui.components;
-
-import javax.swing.*;
-import java.awt.*;
-
-public class AdminPanel extends JPanel {
-    public AdminPanel() {
-        setLayout(new BorderLayout());
-        JTabbedPane adminTabs = new JTabbedPane();
-        adminTabs.addTab("Audit Logs", new AuditLogPanel());
-        adminTabs.addTab("Login History", new LoginHistoryPanel());
-        add(adminTabs, BorderLayout.CENTER);
-    }
 }
